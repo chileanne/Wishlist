39e7b44e539645a0ec38d5c52cb2d08fb80bb81d
ekpei <anthony4arsenal@gmail.com>
2025-01-08 14:10:22 +0100

    Initial Commit

---
+++ Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by user on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
